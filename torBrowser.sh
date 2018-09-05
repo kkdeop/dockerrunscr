92d0eb37aef3a2ecbbf29073484bbd614a7cb8cf
root <root@hostname.domain>
2018-09-06 03:28:54 +0400

    [kkdeop] add downloads to torBrowser.sh

--- torBrowser.sh
+++ torBrowser.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # run tor browser in docker container
+mkdir /tmp/Downloads
+chmod 777 /tmp/Downloads
+echo "Download files to: /home/anon/Dowloads"
+echo "Path to downloads on local: /tmp/Downloads"
 xhost +local: 
-docker run -d -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix paulczar/torbrowser
+docker run -i -t --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /tmp/Downloads:/home/anon/Downloads paulczar/torbrowser
